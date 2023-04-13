3cb55ac2aa6c0298ae80d805b4bb14fdd69c92ad
Meam1c <matvey.jilkevich@gmail.com>
2023-04-13 18:05:12 +0300

    Add project files.

---
+++ BusOOPProject/bus.h
@@ -0,0 +1,24 @@
+#include"main.h"
+#pragma once
+
+class Bus {
+public:
+	string brand;
+	string number;
+	int  price;
+	int capacity;
+
+	Bus() {
+		brand = " no brand";
+		 number = "XXXXPM&";
+	      price = 0;
+		  capacity = 0;;
+
+	}
+	Bus(string brnd, string num, int prc, int cpct) {
+		brand = brnd;
+		number = num;
+		price = prc;
+		capacity = cpct;
+	}
+};
